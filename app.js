bfa1731a5d3605c12bc6a25c03d383b28d3aa9d1
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-08 10:08:49 +0000

    Add reformat and move function randomAge out of the loop

--- app.js
+++ app.js
@@ -1,137 +1,136 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const genders =['F','M'];
+const genders = ["F", "M"];
 console.log(genders);
 
 const maleNames = [
-    'Keneth',
-    'Galen',
-    'Manuel',
-    'Sam',
-    'Raymon',
-    'Kent',
-    'Leon',
-    'Rich',
-    'Lucius',
-    'Walton',
-    'Neville',
-    'Wilson',
-    'Bob',
-    'Emory',
-    'Joseph',
-    'Kenneth',
-    'Sanford',
-    'Alvin',
-    'Arron',
-    'Reuben',
-    'Erasmo',
-    'Frederic',
-    'Miles',
-    'Florencio',
-    'Alphonso',
-    'Archie',
-    'Gino',
-    'Lamar',
-    'Newton',
-    'Orville',    
+  "Keneth",
+  "Galen",
+  "Manuel",
+  "Sam",
+  "Raymon",
+  "Kent",
+  "Leon",
+  "Rich",
+  "Lucius",
+  "Walton",
+  "Neville",
+  "Wilson",
+  "Bob",
+  "Emory",
+  "Joseph",
+  "Kenneth",
+  "Sanford",
+  "Alvin",
+  "Arron",
+  "Reuben",
+  "Erasmo",
+  "Frederic",
+  "Miles",
+  "Florencio",
+  "Alphonso",
+  "Archie",
+  "Gino",
+  "Lamar",
+  "Newton",
+  "Orville"
 ];
-console.log('maleNames:', maleNames); 
+console.log("maleNames:", maleNames);
 
 const femaleNames = [
-    'Sharice',
-    'Malena',
-    'Jeana',
-    'Bernarda',
-    'Marvel',
-    'Arletta',
-    'Dorene',
-    'Clarisa',
-    'Rochelle',
-    'Micha',
-    'Genoveva',
-    'Valerie',
-    'Minna',
-    'Cheryl',
-    'Aileen',
-    'Fanny',
-    'Kandy',
-    'Jesusita',
-    'Sherise',
-    'Jade',
-    'Zula',
-    'Riva',
-    'Bobette',
-    'Carlee',
-    'Delphia',
-    'Annika',
-    'Cherelle',
-    'Tanna',
-    'Chassidy',
-    'Natashia',
+  "Sharice",
+  "Malena",
+  "Jeana",
+  "Bernarda",
+  "Marvel",
+  "Arletta",
+  "Dorene",
+  "Clarisa",
+  "Rochelle",
+  "Micha",
+  "Genoveva",
+  "Valerie",
+  "Minna",
+  "Cheryl",
+  "Aileen",
+  "Fanny",
+  "Kandy",
+  "Jesusita",
+  "Sherise",
+  "Jade",
+  "Zula",
+  "Riva",
+  "Bobette",
+  "Carlee",
+  "Delphia",
+  "Annika",
+  "Cherelle",
+  "Tanna",
+  "Chassidy",
+  "Natashia"
 ];
-console.log('femaleNames:', femaleNames); 
-
+console.log("femaleNames:", femaleNames);
 
 const lastNames = [
-    'Smith',
-    'Johnson',
-    'Williams',
-    'Jones',
-    'Brown',
-    'Davis',
-    'Miller',
-    'Wilson',
-    'Moore',
-    'Taylor',
-    'Anderson',
-    'Thomas',
-    'Jackson',
-    'White',
-    'Harris',
-    'Martin',
-    'Thompson',
-    'Garcia',
-    'Martinez',
-    'Robinson',
-    'Clark',
-    'Rodriguez',
-    'Lewis',
-    'Lee',
-    'Walker',
+  "Smith",
+  "Johnson",
+  "Williams",
+  "Jones",
+  "Brown",
+  "Davis",
+  "Miller",
+  "Wilson",
+  "Moore",
+  "Taylor",
+  "Anderson",
+  "Thomas",
+  "Jackson",
+  "White",
+  "Harris",
+  "Martin",
+  "Thompson",
+  "Garcia",
+  "Martinez",
+  "Robinson",
+  "Clark",
+  "Rodriguez",
+  "Lewis",
+  "Lee",
+  "Walker"
 ];
-console.log('lastNames:', lastNames); 
+console.log("lastNames:", lastNames);
 
 const randChoice = arr => {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+function randomAge(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-  
+
 const people = [];
-  
-for (let i=0; i<20; i++) {
-    const gender = randChoice(genders);
 
-    let firstName;
-    if(gender === 'M') {
-        firstName = randChoice(maleNames);
-    } else {
-      firstName = randChoice(femaleNames);
-    }
-    const lastName = randChoice(lastNames);
-        
-    const age = randomAge(18,78);
-    console.log(age);
+for (let i = 0; i < 20; i++) {
+  const gender = randChoice(genders);
 
-    function randomAge(min, max) { // min and max age 
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      }
+  let firstName;
+  if (gender === "M") {
+    firstName = randChoice(maleNames);
+  } else {
+    firstName = randChoice(femaleNames);
+  }
+  const lastName = randChoice(lastNames);
 
-    const randomPerson = { gender, firstName, lastName, age };
-    people.push(randomPerson);
+  const age = randomAge(18, 78);
+  console.log(age);
+
+  const randomPerson = { gender, firstName, lastName, age };
+  people.push(randomPerson);
 }
 
 const jsonString = JSON.stringify(people);
 
-fs.writeFile('people.json', jsonString, err => {
+fs.writeFile("people.json", jsonString, err => {
   if (err) throw err;
-  console.log('The file has been saved!');
+  console.log("The file has been saved!");
 });
